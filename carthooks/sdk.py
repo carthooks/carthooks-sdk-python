1507a9bb60c4a77e320d0cdf58c70b85da2fa735
laotie <flaboy.cn@gmail.com>
2024-04-13 17:50:06 +0800

    m

--- carthooks/sdk.py
+++ carthooks/sdk.py
@@ -1,7 +1,10 @@
 import requests
+import os
 
 class Result:
     def __init__(self, response):
+        self.trace_id = None
+        self.meta = None
         try:
             self.response = response.json()
             self.data = self.response.get('data')
@@ -18,55 +21,79 @@ class Result:
             self.error = response.text
             self.success = False
 
+    def __getitem__(self, key):
+        return self.data.get(key)
+
     def __str__(self) -> str:
-        return f"CarthooksResult(success={self.success}, data={self.data}, error={self.error}, trace_id={self.trace_id}, meta={self.meta})"
+        return f"CarthooksResult(success={self.success}, data={self.data}, error={self.error})"
 
 class Client:
-    def __init__(self, base_url="https://api.carthooks.com"):
-        self.base_url = base_url
+    def __init__(self):
+        self.base_url = os.getenv('CARTHOOKS_API_URL')
+        if self.base_url == None:
+            self.base_url = "https://api.carthooks.com"
         self.headers = {
             'Content-Type': 'application/json',
         }
 
-    def set_access_token(self, access_token):
+    def setAccessToken(self, access_token):
         self.headers['Authorization'] = f'Bearer {access_token}'
 
-    def get_items(self, app_id, collection_id, limit=20, page=1, **options):
+    def getItems(self, app_id, collection_id, limit=20, page=1, **options):
         options['pagination[page]'] = page
         options['pagination[pageSize]'] = limit
-        response = requests.get(f'{self.base_url}/v1/apps/{app_id}/collections/{collection_id}/items', headers=self.headers, params=options)
+        url = f'{self.base_url}/v1/apps/{app_id}/collections/{collection_id}/items'
+        print(options)
+        response = requests.get(url, headers=self.headers, params=options)
         return Result(response)
     
-    def get_item_by_id(self, app_id, collection_id, item_id, fields):
-        response = requests.get(f'{self.base_url}/v1/apps/{app_id}/collections/{collection_id}/items/{item_id}', headers=self.headers, params={'fields': fields})
+    def getItemById(self, app_id, collection_id, item_id, fields=None):
+        response = requests.get(f'{self.base_url}/v1/apps/{app_id}/collections/{collection_id}/items/{item_id}', headers=self.headers)
         return Result(response)
     
-    def get_submission_token(self, app_id, collection_id, options):
+
+# POST    /open/api/v1/apps/:app_id/collections/:entity_id/items/:row_id/subform/:field_id/     OpenAPI.CreateSubItem
+# PUT     /open/api/v1/apps/:app_id/collections/:entity_id/items/:row_id/subform/:field_id/items/:sub_row_id/:sub_row_id     OpenAPI.UpdateSubItem
+# DELETE  /open/api/v1/apps/:app_id/collections/:entity_id/items/:row_id/subform/:field_id/items/:sub_row_id/:sub_row_id     OpenAPI.DeleteSubItem
+    def createSubItem(self, app_id, collection_id, item_id, field_id, data):
+        response = requests.post(f'{self.base_url}/v1/apps/{app_id}/collections/{collection_id}/items/{item_id}/subform/{field_id}', headers=self.headers, json={'data': data})
+        return Result(response)
+    
+    def updateSubItem(self, app_id, collection_id, item_id, field_id, sub_item_id, data):
+        print("data", data)
+        response = requests.put(f'{self.base_url}/v1/apps/{app_id}/collections/{collection_id}/items/{item_id}/subform/{field_id}/items/{sub_item_id}', headers=self.headers, json={'data': data})
+        return Result(response)
+    
+    def deleteSubItem(self, app_id, collection_id, item_id, field_id, sub_item_id):
+        response = requests.delete(f'{self.base_url}/v1/apps/{app_id}/collections/{collection_id}/items/{item_id}/subform/{field_id}/items/{sub_item_id}', headers=self.headers)
+        return Result(response)
+    
+    def getSubmissionToken(self, app_id, collection_id, options):
         response = requests.post(f'{self.base_url}/v1/apps/{app_id}/collections/{collection_id}/submission-token', headers=self.headers, json=options)
         return Result(response)
     
-    def update_submission_token(self, app_id, collection_id, item_id, options):
+    def updateSubmissionToken(self, app_id, collection_id, item_id, options):
         response = requests.post(f'{self.base_url}/v1/apps/{app_id}/collections/{collection_id}/items/{item_id}/update-token', headers=self.headers, json=options)
         return Result(response)
     
-    def create_item(self, app_id, collection_id, data):
+    def createItem(self, app_id, collection_id, data):
         response = requests.post(f'{self.base_url}/v1/apps/{app_id}/collections/{collection_id}/items', headers=self.headers, json={'data': data})
         return Result(response)
     
-    def update_item(self, app_id, collection_id, item_id, data):
+    def updateItem(self, app_id, collection_id, item_id, data):
         response = requests.put(f'{self.base_url}/v1/apps/{app_id}/collections/{collection_id}/items/{item_id}', headers=self.headers, json={'data': data})
         return Result(response)
     
-    def lock_item(self, app_id, collection_id, item_id, lock_timeout=600, lock_id=None, subject=None):
+    def lockItem(self, app_id, collection_id, item_id, lock_timeout=600, lock_id=None, subject=None):
         response = requests.post(f'{self.base_url}/v1/apps/{app_id}/collections/{collection_id}/items/{item_id}/lock', 
                                  headers=self.headers, json={'lockTimeout': lock_timeout, 'lockId': lock_id, 'lockSubject': subject}) 
         return Result(response)
     
-    def unlock_item(self, app_id, collection_id, item_id, lock_id=None):
+    def unlockItem(self, app_id, collection_id, item_id, lock_id=None):
         response = requests.post(f'{self.base_url}/v1/apps/{app_id}/collections/{collection_id}/items/{item_id}/unlock', headers=self.headers, json={'lockId': lock_id})
         return Result(response)
     
-    def delete_item(self, app_id, collection_id, item_id):
+    def deleteItem(self, app_id, collection_id, item_id):
         response = requests.delete(f'{self.base_url}/v1/apps/{app_id}/collections/{collection_id}/items/{item_id}', headers=self.headers)
         return Result(response)
     
