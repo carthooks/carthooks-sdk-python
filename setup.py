8605b530887f0d8bef04efc387faa51f2fd54ff4
laotie <flaboy.cn@gmail.com>
2025-09-06 15:45:39 +0800

    Bump version to 0.1.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='carthooks',
-    version='0.1.7',
+    version='0.1.8',
     packages=find_packages(),
     description='Carthooks Python SDK',
     long_description=open('README.md').read(),
